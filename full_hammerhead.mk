64d9d70423a115bf34fc9b3638e6a44d07099957
task650 <task650@gmail.com>
2013-12-05 18:41:33 -0800

    Gotta add a couple things tha ghetto way :)

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -20,6 +20,16 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
+# Extra Apps and files
+PRODUCT_COPY_FILES += \
+    vendor/lge/hammerhead/Alert-SonarMerge.mp3:system/media/audio/notifications/Alert-SonarMerge.mp3 \
+    vendor/lge/hammerhead/CyanPing.ogg:system/media/audio/notifications/CyanPing.ogg \
+    vendor/lge/hammerhead/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
+    vendor/lge/hammerhead/apple_smsreceived.ogg:system/media/audio/notifications/apple_smsreceived.ogg \
+    vendor/lge/hammerhead/IphoneCellSoundMerge.mp3:system/media/audio/ringtones/IphoneCellSoundMerge.mp3 \
+    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk \
+    vendor/lge/hammerhead/com.aokp.ota.apk:system/app/com.aokp.ota.apk
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
